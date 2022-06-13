60ce668a39cb5ada5d645b79d5380a5865540c60
Remedan <97916325+Remsaf@users.noreply.github.com>
2022-06-13 17:35:43 +0300

    Add files via upload

---
+++ Rock-Paper-Scissors_Game/main.py
@@ -0,0 +1,23 @@
+#Rock-Paper-Scissors_Game
+from random import randint
+
+# moves for the player
+moves = ["rock", "paper", "scissors"]
+
+while True:
+  computer = moves[randint(0,2)]
+  player = input("rock, paper or scissors? (or end the game)").lower()
+  
+  if player == "end the game":
+    print("The game has ended.")
+    break
+  elif player == computer:
+    print("Tie!.")
+  elif player == "rock":
+    if computer == "paper":
+      print("You lose!.", computer, "beats", player)
+    else:
+      print("You win!.", player, "beats", computer)
+      
+    
+  
